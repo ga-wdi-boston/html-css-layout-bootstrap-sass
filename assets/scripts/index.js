7da45ff00c5ff37556929eb2f723efc823afd821
MicFin <mrfinneran@gmail.com>
2017-01-23 12:41:00 -0500

    Update from `browser-template` correction
    Remove `require('./example')` which should have been removed at template update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,3 @@ $(() => {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-require('./example');
